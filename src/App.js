7edeb00b76798c17d1ee40db8b583997b7784bd9
saadbess <saad@koubeissi.com>
2019-09-22 10:15:08 +0100

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>ProjectPlan</h1>
+          <Navbar />
         </div>
       </BrowserRouter>
     )
   }
 }
 
-export default App;
+export default App
 
